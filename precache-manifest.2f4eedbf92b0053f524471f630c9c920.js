454290deccd1f5218a4e41d228abc33ef64261af
yanchen01 <ychen01@bu.edu>
2020-08-18 12:19:29 -0400

    Updates

---
+++ precache-manifest.2f4eedbf92b0053f524471f630c9c920.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984322318402231726c82d165d719651",
+    "url": "/personal-web/index.html"
+  },
+  {
+    "revision": "935f1b62ff730fe6692a",
+    "url": "/personal-web/static/css/main.b3f291d8.chunk.css"
+  },
+  {
+    "revision": "fc0f94e9ecfc2fda7e13",
+    "url": "/personal-web/static/js/2.8153a6c7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/personal-web/static/js/2.8153a6c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "935f1b62ff730fe6692a",
+    "url": "/personal-web/static/js/main.d3389395.chunk.js"
+  },
+  {
+    "revision": "df052472833950bd4126",
+    "url": "/personal-web/static/js/runtime-main.ad575770.js"
+  },
+  {
+    "revision": "b9ff91ad88d6e7c7ae946e887564fa16",
+    "url": "/personal-web/static/media/Yan_Resume.b9ff91ad.pdf"
+  },
+  {
+    "revision": "bb8cdfd39b977bcb228477256f91e479",
+    "url": "/personal-web/static/media/burger-builder.bb8cdfd3.png"
+  },
+  {
+    "revision": "aa7fd3ed25d4f0bd93e5e872f65a9223",
+    "url": "/personal-web/static/media/portrait.aa7fd3ed.png"
+  },
+  {
+    "revision": "ea714361e0ef54d7233b6e989ce22629",
+    "url": "/personal-web/static/media/sapflow.ea714361.png"
+  },
+  {
+    "revision": "829597ec95d1ff85e37b7b74767d230f",
+    "url": "/personal-web/static/media/sentithink.829597ec.png"
+  },
+  {
+    "revision": "56d910d88cb207ef14818b14216ee848",
+    "url": "/personal-web/static/media/terrier-exchange.56d910d8.png"
+  },
+  {
+    "revision": "0d9689195e734af2277ec541b85b4bcb",
+    "url": "/personal-web/static/media/yelp-camp.0d968919.png"
+  }
+]);
